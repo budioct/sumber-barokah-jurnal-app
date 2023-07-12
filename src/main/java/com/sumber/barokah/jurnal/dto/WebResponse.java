bddb7c51c9d717215d3c92d3051c2dc68d9b661c
Budhi Octaviansyah <budioct@gmail.com>
2023-07-12 13:14:41 +0700

    create response body

---
+++ src/main/java/com/sumber/barokah/jurnal/dto/WebResponse.java
@@ -0,0 +1,40 @@
+package com.sumber.barokah.jurnal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+
+    // class ini dibuat untuk response body, Sebagai wrapper Type child
+    // karena response berubah2 kita buat sama menjadi Generic Class
+    // class ini dibuat untuk response body, Success dan Error.
+    // ketika client consume/hit endpoint akan di beri response seperti format di bawah
+
+    /**
+     * bentuk response body :
+     * Success
+     *    {
+     *      data: {
+     *              // data
+     *            }
+     *     }
+     *
+     * Error
+     *     {
+     *         errors : pesan error
+     *     }
+     */
+
+    private T data;
+
+    private String errors;
+
+    private PagingResponse paging;
+
+}
