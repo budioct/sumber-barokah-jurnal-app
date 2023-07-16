d2065979f76dbad66c9790f54b35bc63c4d9a146
Budhi Octaviansyah <budioct@gmail.com>
2023-07-16 22:47:14 +0700

    update response. all http method, api category

--- src/main/java/com/sumber/barokah/jurnal/controller/CategoryController.java
+++ src/main/java/com/sumber/barokah/jurnal/controller/CategoryController.java
@@ -5,7 +5,9 @@ import com.sumber.barokah.jurnal.dto.master.CategoryResponse;
 import com.sumber.barokah.jurnal.dto.master.CreateCategoryRequest;
 import com.sumber.barokah.jurnal.dto.master.UpdateCategoryRequest;
 import com.sumber.barokah.jurnal.service.CategoryService;
+import com.sumber.barokah.jurnal.utilities.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +28,12 @@ public class CategoryController {
 
         CategoryResponse categoryResponse = categoryService.create(request);
 
-        return WebResponse.<CategoryResponse>builder().data(categoryResponse).build();
+        return WebResponse.<CategoryResponse>builder()
+                .data(categoryResponse)
+                .status(HttpStatus.OK)
+                .status_code(Constants.OK)
+                .message(Constants.CREATE_MESSAGE)
+                .build();
 
     }
 
@@ -38,7 +45,12 @@ public class CategoryController {
 
         List<CategoryResponse> categoryResponses = categoryService.listCategory();
 
-        return WebResponse.<List<CategoryResponse>>builder().data(categoryResponses).build();
+        return WebResponse.<List<CategoryResponse>>builder()
+                .data(categoryResponses)
+                .status(HttpStatus.OK)
+                .status_code(Constants.OK)
+                .message(Constants.ITEM_EXIST_MESSAGE)
+                .build();
 
     }
 
@@ -50,7 +62,12 @@ public class CategoryController {
 
         CategoryResponse categoryResponse = categoryService.get(id);
 
-        return WebResponse.<CategoryResponse>builder().data(categoryResponse).build();
+        return WebResponse.<CategoryResponse>builder()
+                .data(categoryResponse)
+                .status(HttpStatus.OK)
+                .status_code(Constants.OK)
+                .message(Constants.ITEM_EXIST_MESSAGE)
+                .build();
 
     }
 
@@ -65,7 +82,12 @@ public class CategoryController {
         request.setCategoryId(id);
         CategoryResponse categoryResponse = categoryService.update(request);
 
-        return WebResponse.<CategoryResponse>builder().data(categoryResponse).build();
+        return WebResponse.<CategoryResponse>builder()
+                .data(categoryResponse)
+                .status(HttpStatus.OK)
+                .status_code(Constants.OK)
+                .message(Constants.UPDATE_MESSAGE)
+                .build();
     }
 
     @DeleteMapping(
@@ -76,7 +98,12 @@ public class CategoryController {
 
         categoryService.delete(id);
 
-        return WebResponse.<String>builder().data("OK").build();
+        return WebResponse.<String>builder()
+                .data("")
+                .status(HttpStatus.OK)
+                .status_code(Constants.OK)
+                .message(Constants.DELETE_MESSAGE)
+                .build();
 
     }
 
