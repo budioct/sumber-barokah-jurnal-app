2ac5ff9a5d04755768bbeace6117aebba2fc3dca
Budhi Octaviansyah <budioct@gmail.com>
2023-08-23 14:35:08 +0700

    setup config migration db

---
+++ src/main/resources/db/migration/V20230801__skemadb.sql
@@ -0,0 +1,101 @@
+create table s_roles
+(
+    id   varchar(36),
+    name varchar(100) not null,
+    primary key (id),
+    unique (name)
+);
+
+create table s_users
+(
+    id       varchar(36),
+    username varchar(100) not null,
+    active   boolean      not null,
+    id_role  varchar(36)  not null,
+    primary key (id),
+    unique (username),
+    foreign key (id_role) references s_roles (id)
+);
+
+create table s_users_passwords
+(
+    id_user       varchar(36),
+    user_password varchar(255) not null,
+    primary key (id_user),
+    foreign key (id_user) references s_users (id)
+);
+
+create table s_permissions
+(
+    id               varchar(36),
+    permission_label varchar(100) not null,
+    permission_value varchar(100) not null,
+    primary key (id),
+    unique (permission_value)
+);
+
+create table s_roles_permissions
+(
+    id_role       varchar(36),
+    id_permission varchar(36),
+    primary key (id_role, id_permission),
+    foreign key (id_role) references s_roles (id),
+    foreign key (id_permission) references s_permissions (id)
+);
+
+
+CREATE TABLE oauth2_registered_client
+(
+    id                            varchar(255)                            NOT NULL,
+    client_id                     varchar(255)                            NOT NULL,
+    client_id_issued_at           timestamp     DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    client_secret                 varchar(255)  DEFAULT NULL,
+    client_secret_expires_at      timestamp     DEFAULT NULL,
+    client_name                   varchar(255)                            NOT NULL,
+    client_authentication_methods varchar(1000)                           NOT NULL,
+    authorization_grant_types     varchar(1000)                           NOT NULL,
+    redirect_uris                 varchar(1000) DEFAULT NULL,
+    scopes                        varchar(1000)                           NOT NULL,
+    client_settings               varchar(2000)                           NOT NULL,
+    token_settings                varchar(2000)                           NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE oauth2_authorization
+(
+    id                            varchar(255) NOT NULL,
+    registered_client_id          varchar(255) NOT NULL,
+    principal_name                varchar(255) NOT NULL,
+    authorization_grant_type      varchar(255) NOT NULL,
+    authorized_scopes             varchar(1000) DEFAULT NULL,
+    attributes                    blob          DEFAULT NULL,
+    state                         varchar(500)  DEFAULT NULL,
+    authorization_code_value      blob          DEFAULT NULL,
+    authorization_code_issued_at  timestamp     DEFAULT NULL,
+    authorization_code_expires_at timestamp     DEFAULT NULL,
+    authorization_code_metadata   varchar(2000) DEFAULT NULL,
+    access_token_value            blob          DEFAULT NULL,
+    access_token_issued_at        timestamp     DEFAULT NULL,
+    access_token_expires_at       timestamp     DEFAULT NULL,
+    access_token_metadata         varchar(2000) DEFAULT NULL,
+    access_token_type             varchar(255)  DEFAULT NULL,
+    access_token_scopes           varchar(1000) DEFAULT NULL,
+    refresh_token_value           blob          DEFAULT NULL,
+    refresh_token_issued_at       timestamp     DEFAULT NULL,
+    refresh_token_expires_at      timestamp     DEFAULT NULL,
+    refresh_token_metadata        varchar(2000) DEFAULT NULL,
+    oidc_id_token_value           blob          DEFAULT NULL,
+    oidc_id_token_issued_at       timestamp     DEFAULT NULL,
+    oidc_id_token_expires_at      timestamp     DEFAULT NULL,
+    oidc_id_token_metadata        varchar(2000) DEFAULT NULL,
+    oidc_id_token_claims          varchar(2000) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE oauth2_authorization_consent
+(
+    registered_client_id varchar(255)  NOT NULL,
+    principal_name       varchar(255)  NOT NULL,
+    authorities          varchar(1000) NOT NULL,
+    PRIMARY KEY (registered_client_id, principal_name)
+);
---
+++ src/main/resources/db/migration/V20230802__dataskemadb.sql
@@ -0,0 +1,51 @@
+insert into s_roles (id, name)
+values ('r001', 'staff');
+
+insert into s_roles (id, name)
+values ('r002', 'manager');
+
+insert into s_users (id, username, active, id_role)
+values ('u001', 'budhi', true, 'r001');
+
+insert into s_users (id, username, active, id_role)
+values ('u002', 'mamat', true, 'r002');
+
+-- password : rahasia
+insert into s_users_passwords (id_user, user_password)
+values ('u001', '$2a$10$CM3UsAi9Miyos5rPqImbquiFsfexVx26RoxtBYr7TQvnKaW.5OLcy');
+
+-- password : rahasia
+insert into s_users_passwords (id_user, user_password)
+values ('u002', '$2a$10$CM3UsAi9Miyos5rPqImbquiFsfexVx26RoxtBYr7TQvnKaW.5OLcy');
+
+insert into s_permissions (id, permission_label, permission_value)
+values ('p001', 'Lihat Data Transaksi', 'VIEW_TRANSAKSI');
+
+insert into s_permissions (id, permission_label, permission_value)
+values ('p002', 'Edit Data Transaksi', 'EDIT_TRANSAKSI');
+
+insert into s_roles_permissions (id_role, id_permission)
+values ('r001', 'p001');
+
+insert into s_roles_permissions (id_role, id_permission)
+values ('r002', 'p001');
+
+insert into s_roles_permissions (id_role, id_permission)
+values ('r002', 'p002');
+
+-- client name : client1
+-- client secret : abcd
+-- grant type : authorization code with PKCE
+insert into oauth2_registered_client (id, client_id, client_id_issued_at, client_secret, client_secret_expires_at,
+                                      client_name, client_authentication_methods, authorization_grant_types,
+                                      redirect_uris,
+                                      scopes,
+                                      client_settings,
+                                      token_settings)
+values ('432f7b10-c3cd-4084-ac19-9ee068a7b435', 'client1', '2023-03-10 13:41:12.558667',
+        '$2y$10$QmpDqxEmx0vdnZuvS8R3U.lCvLjwFGK44IFrbveDesIhgtvegC4im', null,
+        'client1', 'client_secret_basic', 'authorization_code,refresh_token',
+        'http://127.0.0.1:8080/login/oauth2/code/users-client-oidc,http://127.0.0.1:8080/authorized',
+        'openid,profile,message.read,message.write',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":true,"settings.client.require-authorization-consent":true}',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000]}');
